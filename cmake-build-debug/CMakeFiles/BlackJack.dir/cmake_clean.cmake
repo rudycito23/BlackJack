71485b5521e2f4fb0af48e03b2322f3abc8ac8c1
rudycito23 <88415585+rudycito23@users.noreply.github.com>
2022-05-22 13:32:25 -0500

    First commit - Black Jack 21

---
+++ cmake-build-debug/CMakeFiles/BlackJack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BlackJack"
+  "BlackJack.pdb"
+  "CMakeFiles/BlackJack.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BlackJack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
